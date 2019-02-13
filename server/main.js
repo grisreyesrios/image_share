5643f9aec2869c2065897a18031ff1dcac9e0f04
Grisell Reyes Rios <grisell.reyes.rios@gmail.com>
2019-02-12 23:53:24 -0600

    Adding public/pictures and testing on the server

--- server/main.js
+++ server/main.js
@@ -3,3 +3,11 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+if (Meteor.isClient) {
+  console.log("I am the client");
+}
+
+if (Meteor.isServer) {
+  console.log("I am the server");
+}
