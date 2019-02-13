3d163e84f99d512dc9ddc0b4ad992d63541830a2
Grisell Reyes Rios <grisell.reyes.rios@gmail.com>
2019-02-13 16:04:57 -0600

    Adding more pictures and modifing Bootstrap grid

--- client/main.js
+++ client/main.js
@@ -16,6 +16,30 @@ import './main.html';
     img_src:'./wallpaper3.jpeg',
     img_alt:'Smokecolors'
   },
+  {
+    img_src:'./wallpaper4.jpeg',
+    img_alt:'Snowing on the street'
+  },
+  {
+    img_src:'./wallpaper5.jpeg',
+    img_alt:'Taj Mahal'
+  },
+  {
+    img_src:'./wallpaper6.jpeg',
+    img_alt:'Ruins'
+  },
+  {
+    img_src:'./wallpaper7.jpeg',
+    img_alt:'Pool Party'
+  },
+  {
+    img_src:'./wallpaper8.jpeg',
+    img_alt:'Hong Kong'
+  },
+  {
+    img_src:'./wallpaper9.jpeg',
+    img_alt:'Bali, Indonesia'
+  },
   ];
 
   Template.images.helpers({images:img_data});
