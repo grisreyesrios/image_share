1bab24281dfb3ac46d4dc77d9b348db063124e6f
Grisell Reyes Rios <grisell.reyes.rios@gmail.com>
2019-02-13 00:32:53 -0600

    Adding iteration on template in connection with main.js

--- client/main.js
+++ client/main.js
@@ -3,21 +3,20 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-if (Meteor.isClient) {
   var img_data = [
   {
-    img_src:"wallpaper1.jpg",
-    img_alt:"womenscubadiving"
+    img_src:'wallpaper1.jpg',
+    img_alt:'womenscubadiving'
   },
   {
-    img_src:"wallpaper2.jpg",
-    img_alt:"Graffiti"
+    img_src:'wallpaper2.jpg',
+    img_alt:'Graffiti'
   },
   {
-    img_src:"wallpaper3.jpg",
-    img_alt:"Smokecolors"
+    img_src:'wallpaper3.jpg',
+    img_alt:'Smokecolors'
   },
   ];
 
-  Template.images.helpers(img_data);
-}
+  Template.images.helpers({images:img_data});
+
