31981bc37ca745931d4e5f8088349cbe9bd900dc
Grisell Reyes Rios <grisell.reyes.rios@gmail.com>
2019-02-13 00:17:32 -0600

    Adding extra images to render to template through main.js

--- client/main.js
+++ client/main.js
@@ -3,3 +3,21 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
+if (Meteor.isClient) {
+  var img_data = [
+  {
+    img_src:"wallpaper1.jpg",
+    img_alt:"womenscubadiving"
+  },
+  {
+    img_src:"wallpaper2.jpg",
+    img_alt:"Graffiti"
+  },
+  {
+    img_src:"wallpaper3.jpg",
+    img_alt:"Smokecolorr"
+  },
+  ];
+
+  Template.images.helpers(img_data);
+}
